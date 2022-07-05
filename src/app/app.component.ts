0c3a382e0be496543060d09f0011aaa498e21acd
Anvarmirzo <64534079+Anvarmirzo@users.noreply.github.com>
2022-07-05 18:00:01 +0500

    added toolbar to app component (#1)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {MatDialog} from "@angular/material/dialog";
+import {DialogComponent} from "./components/dialog/dialog.component";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
-  title = 'crud';
+  constructor(private dialog: MatDialog) {
+  }
+
+  openDialog() {
+    this.dialog.open(DialogComponent, {
+      width: '30%',
+    });
+  }
 }
