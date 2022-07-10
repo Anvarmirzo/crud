4d31700419182aa443328a67a10fbf1fc4d0becf
Anvarmirzo <www.latvienhoasadrizeges@gmail.com>
2022-07-10 21:22:12 +0500

    added update-delete methods

--- src/app/components/dialog/dialog.component.ts
+++ src/app/components/dialog/dialog.component.ts
@@ -1,7 +1,8 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, Inject, OnInit} from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
-import {MatDialogRef} from '@angular/material/dialog';
+import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material/dialog';
 import {ApiService} from '../../service/api.service';
+import {IProduct} from '../../models/product';
 
 @Component({
   selector: 'app-dialog',
@@ -12,7 +13,12 @@ export class DialogComponent implements OnInit {
   freshnessList = ['Brand New', 'Like New', 'Used', 'Very Used'];
   productForm!: FormGroup;
 
-  constructor(private formBuilder: FormBuilder, private apiService: ApiService, private dialogRef: MatDialogRef<DialogComponent>) {
+  constructor(
+    private formBuilder: FormBuilder,
+    @Inject(MAT_DIALOG_DATA) public editData: IProduct,
+    private apiService: ApiService,
+    private dialogRef: MatDialogRef<DialogComponent>
+  ) {
   }
 
   ngOnInit() {
@@ -25,20 +31,39 @@ export class DialogComponent implements OnInit {
       date: ['', Validators.required]
     });
 
+    if (this.editData) {
+      const {id, ...data} = this.editData;
+      this.productForm.setValue(data)
+    }
   }
 
   onSubmit() {
     if (this.productForm.valid) {
-      this.apiService.postProduct(this.productForm.value).subscribe({
-        next: (data) => {
-          this.dialogRef.close();
-          this.productForm.reset();
-          alert('Product added successfully')
-        },
-        error: (err) => {
-          console.log(err)
-        }
-      })
+      this.editData ? this.updateProduct() : this.postProduct()
     }
+  }
+
+  updateProduct() {
+    this.apiService.updateProduct({id: this.editData.id, ...this.productForm.value}).subscribe({
+      next: (product) => {
+        this.dialogRef.close(product);
+        this.productForm.reset();
+      },
+      error: (err) => {
+        console.log(err)
+      }
+    });
+  }
+
+  postProduct() {
+    this.apiService.postProduct(this.productForm.value).subscribe({
+      next: (product) => {
+        this.dialogRef.close(product);
+        this.productForm.reset();
+      },
+      error: (err) => {
+        console.log(err)
+      }
+    })
   }
 }
--- src/app/service/api.service.ts
+++ src/app/service/api.service.ts
@@ -6,16 +6,23 @@ import {IProduct} from '../models/product';
   providedIn: 'root'
 })
 export class ApiService {
-
   constructor(private http: HttpClient) {
   }
 
-  postProduct(product: IProduct) {
+  postProduct(product: Omit<IProduct, 'id'>) {
     return this.http.post<IProduct>('http://localhost:3000/products', product);
   }
 
   getProducts() {
     return this.http.get<IProduct[]>('http://localhost:3000/products');
+  }
+
+  updateProduct({id, ...product}: IProduct) {
+    return this.http.put<IProduct>(`http://localhost:3000/products/${id}`, product)
+  }
+
+  deleteProduct(id: number) {
+    return this.http.delete<IProduct>(`http://localhost:3000/products/${id}`)
   }
 }
 
