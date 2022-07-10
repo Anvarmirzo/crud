e7ed24ee75669ae6e5cae04809dbb3d709dca8fe
Anvarmirzo <64534079+Anvarmirzo@users.noreply.github.com>
2022-07-10 21:53:41 +0500

    changed db link (#9)

--- src/app/service/api.service.ts
+++ src/app/service/api.service.ts
@@ -6,23 +6,25 @@ import {IProduct} from '../models/product';
   providedIn: 'root'
 })
 export class ApiService {
+  baseURL = 'https://ng-json-server.herokuapp.com/products';
+
   constructor(private http: HttpClient) {
   }
 
   postProduct(product: Omit<IProduct, 'id'>) {
-    return this.http.post<IProduct>('http://localhost:3000/products', product);
+    return this.http.post<IProduct>(this.baseURL, product);
   }
 
   getProducts() {
-    return this.http.get<IProduct[]>('http://localhost:3000/products');
+    return this.http.get<IProduct[]>(this.baseURL);
   }
 
   updateProduct({id, ...product}: IProduct) {
-    return this.http.put<IProduct>(`http://localhost:3000/products/${id}`, product)
+    return this.http.put<IProduct>(`${this.baseURL}/${id}`, product)
   }
 
   deleteProduct(id: number) {
-    return this.http.delete<IProduct>(`http://localhost:3000/products/${id}`)
+    return this.http.delete<IProduct>(`${this.baseURL}/${id}`)
   }
 }
 
